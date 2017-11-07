7f5b8788194fac4b91b608ad843563638660e7bd
Léa <lea_hennebert@hotmail.fr>
2017-11-07 14:56:15 +0100

    rename /SignUp/App.js in SignUp.js

--- components/SignUp/SignUp.js
+++ components/SignUp/SignUp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 
-export default class App extends React.Component {
+export default class SignUp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
