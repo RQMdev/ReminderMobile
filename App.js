ce9cec3b87a2bb73e39d7594446108822e6e61a0
RQMplays <benoit.jacques.felix@gmail.com>
2017-11-07 15:41:10 +0100

    Move NavigationApp to his own file

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import SignIn from './components/SignIn/SignIn';
-import { StackNavigator } from 'react-navigation';
-
-import Dashboard from './components/Reminder/Dashboard';
-
-const NavigationApp = StackNavigator({
-    SignIn: { screen: SignIn },
-    Dashboard: { screen: Dashboard },
-}, /*{ navigationOptions: {
-        headerStyle: {
-            marginTop: Expo.Constants.statusBarHeight,
-        },
-        headerTitleStyle: {
-            color: 'white',
-        },
-        headerBackTitleStyle: {
-            color: 'white',
-        },
-        headerTintColor: 'white',
-    }}*/
-);
-
+import NavigationApp from './NavigationApp';
 
 export default class GithubApp extends Component {
 
---
+++ NavigationApp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import SignIn from './components/SignIn/SignIn';
+import Dashboard from './components/Reminder/Dashboard';
+
+export const NavigationApp = StackNavigator({
+    SignIn: { screen: SignIn },
+    Dashboard: { screen: Dashboard },
+}, /*{ navigationOptions: {
+        headerStyle: {
+            marginTop: Expo.Constants.statusBarHeight,
+        },
+        headerTitleStyle: {
+            color: 'white',
+        },
+        headerBackTitleStyle: {
+            color: 'white',
+        },
+        headerTintColor: 'white',
+    }}*/
+);
