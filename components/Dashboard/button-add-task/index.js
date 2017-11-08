3109b125bb1de4f164a986922820a31885067352
RQMplays <benoit.jacques.felix@gmail.com>
2017-11-08 10:23:53 +0100

    fix button-add ActionButton

--- components/Dashboard/button-add-task/index.js
+++ components/Dashboard/button-add-task/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ActionButton from 'react-native-action-button';
-//import { Icon } from 'react-native-elements';
 import { APP_COLORS } from './../../../styles/color';
-import { Ionicons } from '@expo/vector-icons';
 
 const ButtonAddTask = ({ onPressCallBack }) => (
   <ActionButton
     buttonColor={APP_COLORS.primaryAction}
-    //icon={<Icon color={APP_COLORS.primaryText} name={'add'} />}
     onPress={() => onPressCallBack()}
-    Ionicons name="add"
+    name="add"
   />
-  
+
 );
 
 export default ButtonAddTask;
