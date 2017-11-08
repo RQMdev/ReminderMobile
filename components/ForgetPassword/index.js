7f3000a610f965cc875c0705d3c3f5ae504c259f
François Massiot <francois.massiot@outlook.fr>
2017-11-08 11:29:43 +0100

    Change image

--- components/ForgetPassword/index.js
+++ components/ForgetPassword/index.js
@@ -9,7 +9,7 @@ export default class ForgotPassword extends React.Component {
             <View style={styles.logoContainer}>
          <Image
               style ={styles.logo}
-             source = {require('../../assets/img/logotype.png')} style = {styles.image}
+             source = {require('../../assets/img/logotype2.png')} style = {styles.image}
              />
 
              <Text style={styles.title}> Récuperer mon mot de passe </Text>
