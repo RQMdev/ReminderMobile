ce10b950019bb36fb6911b367014e59e50fa11dd
Léa <lea_hennebert@hotmail.fr>
2017-11-08 09:48:42 +0100

    Fix navigation

--- components/SignIn/SignInForm/index.js
+++ components/SignIn/SignInForm/index.js
@@ -26,13 +26,13 @@ export default class SignInForm extends Component {
 				  	  />
 				  	  </TouchableOpacity>
 				  	  </View>
-				<TouchableOpacity>
-					<Text style={styles.buttonText} onPress = {() => navigate('ForgetPassword')}>Forget your password ?</Text>
+				<TouchableOpacity onPress = {() => navigate('ForgetPassword')}>
+					<Text style={styles.buttonText}>Forget your password ?</Text>
 				</TouchableOpacity>
  			 	</View>
  			 	<View>
-				<TouchableOpacity>
-					<Text style={styles.buttonText2} onPress = {() => navigate('SignUp')}>Créer un compte</Text>
+				<TouchableOpacity onPress = {() => navigate('SignUp')}>
+					<Text style={styles.buttonText2}>Créer un compte</Text>
 				</TouchableOpacity>
 				</View>
 
