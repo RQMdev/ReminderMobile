96b25841fe24a365753e6bcd0698467e8b0ec7f8
clamenie <claudiamendoza10@gmail.com>
2017-11-08 11:52:24 +0100

    commit rien3

--- components/Dashboard/button-add-task/index.js
+++ components/Dashboard/button-add-task/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ActionButton from 'react-native-action-button';
-<<<<<<< HEAD
-import { Icon } from 'react-native-elements';
-=======
->>>>>>> 11d1e4ef1805c781bb60c98d5e98a306a5f40a77
 import { APP_COLORS } from './../../../styles/color';
 
 const ButtonAddTask = ({ onPressCallBack }) => (
